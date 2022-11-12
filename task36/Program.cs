ccf26b13f69917f5c5882513c681da833173672a
Alina Malyutina <linamalutina1@gmail.com>
2022-11-12 11:53:54 +0300

    add task 36 - sum of elements with odd index

---
+++ task36/Program.cs
@@ -0,0 +1,36 @@
+﻿void FillRandomArray (int [] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+}
+
+int SumOfOddElements (int [] array1)
+{
+int sum = 0;
+    for(int i=0;i<array1.Length;i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + array1[i];
+        }
+    }
+return sum;
+}
+
+Console.WriteLine("This program fill array random numbers and sum elements with odd index");
+int [] arrayRandom = new int [7];
+FillRandomArray(arrayRandom);
+
+Console.WriteLine("Investigating array is: ");
+Console.Write("[");
+for(int i=0;i<arrayRandom.Length;i++)
+{
+    Console.Write($"{arrayRandom[i]}, ");
+}
+Console.WriteLine("]");
+
+int sum1 = SumOfOddElements(arrayRandom);
+
+Console.WriteLine($"Sum of elements with odd index is {sum1}");
