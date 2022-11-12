ee35797cde076e7331dbc308d15b983f678a97d8
Alina Malyutina <linamalutina1@gmail.com>
2022-11-12 12:08:44 +0300

    add task 38 - difference between max and min number

---
+++ task38/Program.cs
@@ -0,0 +1,34 @@
+﻿float maxi(float [] array)
+{
+    float max = array[0];
+    for(int i=0;i<array.Length;i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+    
+float mini(float [] array1)
+{
+    float min = array1[0];
+    for(int i=0;i<array1.Length;i++)
+    {
+        if (array1[i] < min)
+        {
+            min = array1[i];
+        }
+    }
+    return min;
+}  
+
+Console.WriteLine("This program counts the difference between max and min numbers");
+float [] array3 = {1, 5, -3, 3, 8};
+float maxx = maxi(array3);
+float minn = mini(array3);
+float diff = maxx - minn;
+
+Console.WriteLine($"Difference is {diff}");
+
