15c17f25efc77f983575da1c9a529081243e7fa9
Alina Malyutina <linamalutina1@gmail.com>
2022-11-12 11:46:18 +0300

    add task 34 - fill array random numbers and count even numbers in array

---
+++ task34/Program.cs
@@ -0,0 +1,37 @@
+﻿void FillRandomArray (int [] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+int CountEven (int [] array1)
+{
+int count = 0;
+    for(int i=0;i<array1.Length;i++)
+    {
+        if (array1[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+
+Console.WriteLine("This program fill array random three-digit numbers and count quantity even numbers");
+int [] arrayRandom = new int [10];
+FillRandomArray(arrayRandom);
+
+Console.WriteLine("Investigating array is: ");
+Console.Write("[");
+for(int i=0;i<arrayRandom.Length;i++)
+{
+    Console.Write($"{arrayRandom[i]}, ");
+}
+Console.WriteLine("]");
+
+int count1 = CountEven(arrayRandom);
+
+Console.WriteLine($"Here is {count1} even numbers");
